e4befc94b8ea187ae4d8048348455a25c1f3b8b4
insutance <insutance@naver.com>
2024-08-01 22:36:07 +0900

    fix: 통계 데이터 가져오는 부분을 비동기로 처리함으로써 속도 향상

--- velog_hits/crawler.py
+++ velog_hits/crawler.py
@@ -1,5 +1,7 @@
+import asyncio
 import json
 
+import aiohttp
 import pandas as pd
 import requests
 import streamlit as st
@@ -18,6 +20,7 @@ class HitsCrawler:
     self.username = username
     self.url = f"https://velog.io/@{username}/"
     self.access_token = access_token
+    self.headers = {"Authorization": f"Bearer {access_token}"}
 
   def is_exist_user(self):
     response = requests.get(f"https://velog.io/@{self.username}")
@@ -25,7 +28,7 @@ class HitsCrawler:
 
   def get_post_infos(self) -> pd.DataFrame:
     posts = self.get_posts()
-    hits = self.get_hits(posts)
+    hits = asyncio.run(self.get_hits(posts))
 
     df_posts = pd.DataFrame.from_dict(posts)
     df_hits = pd.DataFrame.from_dict(hits)
@@ -53,30 +56,29 @@ class HitsCrawler:
 
     return posts
 
-  def get_hits(self, posts: list) -> list:
-    headers = {"Authorization": f"Bearer {self.access_token}"}
+  async def get_hits(self, posts: list) -> list:
+    async with aiohttp.ClientSession() as session:
+      tasks = [self.get_view_by_post(session, post) for post in posts]
+      hits = await asyncio.gather(*tasks)
+    return hits
 
-    hits = []
-    for post in posts:
-      query = graphql_get_status(post["id"])
-      response = requests.post(
+  async def get_view_by_post(self, session: aiohttp.ClientSession, post: dict) -> dict:
+    query = graphql_get_status(post["id"])
+    async with session.post(
         url="https://v2cdn.velog.io/graphql",
         json=query,
-        headers=headers
-      )
-      response_data = json.loads(response.text)
+        headers=self.headers,
+        ssl=False
+    ) as response:
+      response_data = await response.json()
       try:
-        hits.append(
-          {
-            "id": post["id"],
-            "total": response_data["data"]["getStats"]["total"],
-            # NOTE: 임시 비활성화로 인해 주석 처리, https://github.com/velog-io/velog/issues/22
-            # "latest_count": response_data["data"]["getStats"]["count_by_day"][0]["count"],
-            # "latest_day": response_data["data"]["getStats"]["count_by_day"][0]["day"]
-          }
-        )
+        return {
+          "id": post["id"],
+          "total": response_data["data"]["getStats"]["total"],
+          # NOTE: 임시 비활성화로 인해 주석 처리, https://github.com/velog-io/velog/issues/22
+          # "latest_count": response_data["data"]["getStats"]["count_by_day"][0]["count"],
+          # "latest_day": response_data["data"]["getStats"]["count_by_day"][0]["day"]
+        }
       except TypeError:
         st.error("Access Token이 잘못된 형식이거나 만료 되었을 수 있습니다.")
         st.stop()
-
-    return hits
