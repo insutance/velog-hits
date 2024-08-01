6deeb888a7cffe31010d511af21d59b47377952e
insutance <insutance@naver.com>
2024-08-01 10:46:55 +0900

    fix: posts 가져오는 쿼리에서 사용하지 않는 데이터 안 가져오도록 수정(short_description, thumnail, user)

--- velog_hits/graphql.py
+++ velog_hits/graphql.py
@@ -1,7 +1,7 @@
 def graphql_posts(username, limit, cursor=None) -> dict:
   """블로그 게시물 목록 가져오는 GraphQL"""
   return {
-    "query": "query velogPosts($input: GetPostsInput!) {\n  posts(input: $input) {\n    id\n    title\n    short_description\n    thumbnail\n    user {\n      id\n      username\n      profile {\n        id\n        thumbnail\n        display_name\n      }\n    }\n    url_slug\n    released_at\n    updated_at\n    comments_count\n    tags\n    is_private\n    likes\n  }\n}\n    ",
+    "query": "query velogPosts($input: GetPostsInput!) {\n  posts(input: $input) {\n    id\n    title\n    url_slug\n    released_at\n    updated_at\n    comments_count\n    tags\n    likes\n  }\n}\n    ",
     "variables": {
       "input": {
         "cursor": cursor if cursor is not None else None,
